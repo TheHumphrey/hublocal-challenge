918e3924862cf96d1c89fbe8f8e6d6d4a7bf249e
thehumphrey <fernandob.delima@outlook.com>
2023-01-25 11:33:10 -0300

    feat: get current user by jwt token

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
 import { PrismaService } from './database/prisma.service';
+import { User } from './user/entities/user.entity';
 
 @Controller('app')
 export class AppController {
@@ -12,5 +14,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('companies')
+  async getCompaniesFromUser(@CurrentUser() user: User) {
+    return user;
   }
 }
